e418ba403aef929b53ccc2bcb6a10c831749506f
大平勝也 <k8t2y9.cat@gmail.com>
2019-06-24 18:32:07 +0900

    fix: 外枠の調整

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,23 @@
 class TweetsController < ApplicationController
-    def index
-        @tweets = Tweet.recent
-    end
+  def index
+    @tweets = Tweet.recent
+  end
 
-    def new
-        @tweet = Tweet.new
-    end
+  def new
+    @tweet = Tweet.new
+  end
 
-    def create
-        @tweet = Tweet.new(tweet_params)
-        if @tweet.save
-            redirect_to tweets_path
-        else
-            render 'new'
-        end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+        redirect_to tweets_path
+    else
+        render 'new'
     end
+  end
 
-    private
-        def tweet_params
-            params.require(:tweet).permit(:tweet)
-        end
+  private
+    def tweet_params
+        params.require(:tweet).permit(:tweet)
+    end
 end
